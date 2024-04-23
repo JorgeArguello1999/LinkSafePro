8c8d61f3bec0401835c1e4919f258c8fcc855984
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-04-23 17:48:43 -0500

    feat: Start the home design

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('speed/', include('speed.urls')),
     path('binary/', include('binary.urls')),
     path('urlshortener/', include('urlshortener.urls')),
-    path('/', include('home.urls')),
+    path('', include('home.urls')),
 ]
