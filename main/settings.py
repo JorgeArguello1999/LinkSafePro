cce737e404b94115684d1a0333cd5a2f41789c9e
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-04-23 17:31:10 -0500

    feat: apps created, binary, home and speed

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'textencrypt'
+    'textencrypt',
+    'speed',
+    'binary',
+    'home',
 ]
 
 MIDDLEWARE = [
