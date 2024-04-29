6ffe1fb8b5029d6ab6e9f5390e5a9dfb08b629cd
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-04-29 12:28:13 -0500

    feat: Tengo que pasar a Flask

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from urlshortener import views
 
 urlpatterns = [
-
+    path('', views.front, name="urlshortener"),
 ]
--- urlshortener/views.py
+++ urlshortener/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def front(request):
+    return render(request, 'urlshortener.html')
