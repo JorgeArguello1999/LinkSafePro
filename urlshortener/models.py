528fe3e83e0e1e4da601d8d9c575ecaa07b9b3f9
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-04-30 18:05:58 -0500

    feat: worked on create a login page with a users

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+"""
+class URLSHORTENER(models.Model):
+    id = models.AutoField(primary_key=True)
+    password = models.CharField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+"""
