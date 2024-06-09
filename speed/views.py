d5031bb1fb7c0c4ca7b5288cb3d64ef3919f7054
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-06-09 09:56:15 -0500

    fix: Problem with speed form

--- speed/views.py
+++ speed/views.py
@@ -7,22 +7,35 @@ from tools import speed
 def get(request):
     code = read_code.read_code('speed.py')
     result = None
-    form = forms.CalculoForm()
+    form = forms.CalculoForm(initial={
+        'velocidad_inicial': 0,
+        'velocidad_final': 0,
+        'aceleracion': 0,
+        'tiempo': 0,
+    })
 
     if request.method == 'POST':
         form = forms.CalculoForm(request.POST)
         if form.is_valid():
-            opcion = form.cleaned_data['opcion']
-            velocidad_inicial = float(form.cleaned_data.get('velocidad_inicial'))
-            velocidad_final = float(form.cleaned_data.get('velocidad_final'))
-            aceleracion = float(form.cleaned_data.get('aceleracion'))
-            tiempo = float(form.cleaned_data.get('tiempo'))
-
             try:
-                if opcion == 'v_f': result = speed.calcular_velocidad_final(velocidad_inicial, aceleracion, tiempo)
-                elif opcion == 'v_i': result = speed.calcular_velocidad_inicial(velocidad_final, aceleracion, tiempo)
-                elif opcion == 'a': result = speed.calcular_aceleracion(velocidad_final, velocidad_inicial, tiempo)
-                elif opcion == 't': result = speed.calcular_tiempo(velocidad_final, velocidad_inicial, aceleracion)
+                opcion = form.cleaned_data['opcion']
+                velocidad_inicial = form.cleaned_data.get('velocidad_inicial')
+                velocidad_final = form.cleaned_data.get('velocidad_final')
+                aceleracion = form.cleaned_data.get('aceleracion')
+                tiempo = form.cleaned_data.get('tiempo')
+
+                if opcion == 'v_f': result = speed.calcular_velocidad_final(
+                    float(velocidad_inicial), float(aceleracion), float(tiempo)
+                )
+                elif opcion == 'v_i': result = speed.calcular_velocidad_inicial(
+                    float(velocidad_final), float(aceleracion), float(tiempo)
+                )
+                elif opcion == 'a': result = speed.calcular_aceleracion(
+                    float(velocidad_final), float(velocidad_inicial), float(tiempo)
+                )
+                elif opcion == 't': result = speed.calcular_tiempo(
+                    float(velocidad_final), float(velocidad_inicial), float(aceleracion)
+                )
             except Exception as e:
                 print(e)
                 result = f"Error: {e}"
